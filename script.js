033738cb70dd67de8c7e6607fe77e5b370c0e842
Swetha-techa <118538021+Swetha-techa@users.noreply.github.com>
2023-05-31 11:50:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	arr(sort(a,b) => a-b);
+	let closestsum = infinity;
+	for(let i=0;i<arr.length;i++)
+		{
+			let left = i+1;
+			let right = arr.length-1;
+			while (left<right)
+				{
+					let sum = arr[i] +arr[left]+arr[right];
+					if(Math.abs(sum-target)<Math.abs(closestsum-target))
+					{
+						closestsum = sum;
+					}
+					if(sum<target)
+					{
+						left++;
+					}
+					else
+					{
+						right--;
+					}
+				}
+		}
+  return closestsum;
 }
 
 module.exports = threeSum;
